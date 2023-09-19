92dd95906e8438ccf189163f2c625305fb0014cf
Prashant Malakoti <32639688+prmalakoti@users.noreply.github.com>
2023-09-19 15:49:36 +0530

    Episoda-01

---
+++ app.js
@@ -0,0 +1,19 @@
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")]),
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")])
+
+    //)
+); // nested element of html
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello world from React...!");
+console.log(heading); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+
+console.log(parent);
+root.render(parent);
